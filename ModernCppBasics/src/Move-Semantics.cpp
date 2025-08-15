d73d0484d73803fbd17cf57a8f38618efbb176c6
Yusuf Baha Kama <m_ybaha@hotmail.com>
2025-08-15 16:04:23 +0300

    Additional changes to MoveSemantics

--- ModernCppBasics/src/Move-Semantics.cpp
+++ ModernCppBasics/src/Move-Semantics.cpp
@@ -47,6 +47,16 @@ namespace MoveSemantics {
 
 		}
 
+		TestClassMS(TestClassMS&& inp) noexcept {
+			testData = inp.testData;
+			testString = inp.testString;
+			inp.testData = NULL;
+			inp.testString = "";
+
+			std::cout << "Move constructor called. testData = " << testData << ", testString = " << testString << std::endl;
+
+		}
+
 		void printObj() {
 			std::cout << "printObj called. testData = " << testData << ", testString = " << testString << std::endl;
 		}
@@ -62,13 +72,15 @@ int main() {
 	MoveSemantics::TestClassMS myObj2(3);
 	MoveSemantics::TestClassMS myObj3("Test");
 	MoveSemantics::TestClassMS myObj4(3, "Test");
-	MoveSemantics::TestClassMS myObj5(myObj3);
+	MoveSemantics::TestClassMS myObj5(myObj3); // Copy constructor
 
 	std::cout << "start printing!\n";
 
 	myObj1.printObj();
 
-
+	MoveSemantics::TestClassMS myObj6(std::move(myObj2)); // Move constructor
+	myObj2.printObj(); // myObj2 should be in a valid but unspecified state after move
+	myObj6.printObj();
 
 	return 0;
 }
